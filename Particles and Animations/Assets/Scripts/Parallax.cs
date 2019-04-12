d1922d3458de563fafc47554a578bacc353e8607
devken <31090803+k3nsalazar2012@users.noreply.github.com>
2019-04-12 11:12:13 +0800

    Speedlines and Tap
    Open scene:
    
    Assets/Scenes/Speedlines and Tap.unity

---
+++ Particles and Animations/Assets/Scripts/Parallax.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ParticlesAndAnimations
+{
+    public class Parallax : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed;
+
+        [SerializeField]
+        private Transform[] childImages;
+
+        private void Update()
+        {
+            foreach(Transform t in childImages)
+            {
+                if (t.localPosition.y > 3.34f)
+                    t.localPosition = Vector3.up * -3.34f;
+
+                t.Translate(Vector3.up * speed * Time.deltaTime);               
+            }
+        }
+    }
+}
---
+++ Particles and Animations/Assets/Scripts/Tap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ParticlesAndAnimations
+{
+    public class Tap : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject particle;
+
+        [SerializeField]
+        private new Camera camera;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var mousePos = Input.mousePosition;
+                mousePos.z = 10;
+
+                var position = camera.ScreenToWorldPoint(mousePos);
+                position.z = 0f;
+                GameObject tapEffect = Instantiate(particle);
+               
+                tapEffect.transform.position = position;
+            }
+        }
+    }
+}
